8b86fb470721cb9555ed0c97098ec8c47bd96741
diezil-aint-smart <davidgarretth04@gmail.com>
2026-03-26 23:08:23 -0400

    updated the logic of the main loop and fixed bugs in the show hand, calculate, deck, and  deal card function

--- 21.py
+++ 21.py
@@ -9,6 +9,7 @@ values = {
 
 deck = []
 def create_deck():
+    deck = []
     for suit in suits:
         for rank in ranks:
             deck.append([rank, suit])
@@ -24,19 +25,21 @@ def shuffle_deck(deck):
 
 def deal_card(deck):
 
-    deck.pop(0) 
+    return deck.pop(0) 
 
 card1 = deal_card(deck)
 card2 = deal_card(deck)
 hand = [ card1, card2]
 
+
 def calculate_score(hand):
         score = 0
+        ace_count = 0   
         for card in hand:
             rank = card[0]
-            value  += value[card]
+            score  += values[rank]
             if rank == "Ace":
-                acecout += 1
+                ace_count += 1
         while score > 21 and ace_count > 0:
             score -= 10
             ace_count -= 1
@@ -52,58 +55,64 @@ def calculate_score(hand):
     Requirement: If the score is over 21 and the hand contains an Ace, 
     reduce the score by 10 until the score is <= 21 or no Aces remain.
     """
-    # TODO: Implement scoring logic and Ace adjustment
+player_name = input("whats your name:")
+hide_first_card = False
+def show_hand(player_name, hand, hide_first_card = False):
+    cards = ["??" if hide_first_card and card == hand[0] else f"{card[0]} of {card[1]}"for card in hand]
+    score = f" | Score: {calculate_score(hand)}" if not hide_first_card else ""
+    print(f"{player_name}'s hand: {', '.join(cards)}{score}")
+  
 
-def show_hand(player_name, hand, hide_first_card=False):
- print( f"{player_name}'s hand: , score is {calculate_score(hand)}")
-
- 
-
- """Prints the formatted hand and current score for the user."""
-    # TODO: Print cards. If hide_first_card is True, obscure the first card.
-pass
 
 def play_game():
-    deck = create_deck()
-    shuffle_deck(deck)
-    card1 = deal_card(deck)
-    card2 = deal_card(deck)
-    hand = calculate_score([card1, card2])
-    dealer_card1 = deal_card(deck)
-    dealer_card2 = deal_card(deck)
-    dealer_hand = [dealer_card1, dealer_card2]
-    dealer_score = calculate_score(dealer_hand)
     while True:
-        show_hand("your", hand)
-        print(f"dealer's hand: {dealer_card1} and a hidden card" )
-        if hand > 21:
-            print("you busted. dealer wins the round.")
-            continue
-        elif hand == 21:
-            print("blackjack u win")
-            continue
-        else:
-            user_input = input("type 'hit' to get another card, or 'stand' to hold your hand: ")
-            if user_input.lower() == "hit":
-                new_card = deal_card(deck)
-                hand.append(new_card)
-            elif user_input.lower() == "stand":
-                while dealer_score < 17:
-                    new_card = deal_card(deck)
-                    dealer_hand.append(new_card)
-                    dealer_score = calculate_score(dealer_hand)
-                show_hand("dealer's", dealer_hand)
-                if dealer_score > 21:
-                    print("dealer busted. you win the round.")
-                elif dealer_score > hand:
-                    print("dealer wins the round.")
-                elif dealer_score < hand:
-                    print("you win the round.")
-                else:
-                    print("it's a tie!")
+        deck = create_deck()
+        shuffle_deck(deck)
+        hand = [deal_card(deck), deal_card(deck)]
+        dealer_hand = [deal_card(deck), deal_card(deck)]
+
+        while True:
+            dealer_score = calculate_score(dealer_hand)
+            player_score = calculate_score(hand)
+
+            show_hand("player", hand)
+            show_hand("dealer", dealer_hand, hide_first_card=True)
+
+            if len(deck) < 20:
+                print("deck is empty. reshuffling.")
+                deck = create_deck()
+                shuffle_deck(deck)
+
+            if player_score > 21:
+                print("you busted. dealer wins the round.")
                 break
+            elif player_score == 21:
+                print("blackjack u win")
+                break
+
             else:
-                print("invalid input. please type 'hit' or 'stand'.")   
+                user_input = input("type 'hit' to get another card, or 'stand' to hold your hand: ")
+                if user_input.lower() == "hit":
+                    new_card = deal_card(deck)
+                    hand.append(new_card)
+                elif user_input.lower() == "stand":
+                    while dealer_score < 17:
+                        new_card = deal_card(deck)
+                        dealer_hand.append(new_card)
+                        dealer_score = calculate_score(dealer_hand)
+                    show_hand("dealer's", dealer_hand)
+                    show_hand(player_name, hand)
+                    if dealer_score > 21:
+                        print("dealer busted. you win the round.")
+                    elif dealer_score > player_score:
+                        print("dealer wins the round.")
+                    elif dealer_score < player_score:
+                        print("you win the round.")
+                    else:
+                        print("it's a tie!")
+                    break
+                else:
+                    print("invalid input. please type 'hit' or 'stand'.")   
                 
         
 
